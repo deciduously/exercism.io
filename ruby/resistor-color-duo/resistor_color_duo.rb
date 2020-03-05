5c14963f300eb3b9ddafa3112cefb3c45c861995
Ben Lovy <ben@deciduously.com>
2020-03-05 17:22:47 -0500

    Lint

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,22 +1,10 @@
-=begin
-Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
-`resistor_color_duo_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-duo` directory.
-=end
-
 class ResistorColorDuo
-  private
-
   COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
-  def self.color_code(c)
-    COLORS.index(c)
+  private_class_method def self.color_code(color)
+    COLORS.index(color)
   end
 
-  public
-
   def self.value(arr)
-    self.color_code(arr[0]) * 10 + self.color_code(arr[1])
+    color_code(arr[0]) * 10 + color_code(arr[1])
   end
 end
