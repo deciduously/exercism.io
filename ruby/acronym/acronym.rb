ad0daab3680db3dae2ff3813b2474d7882388cd1
Ben Lovy <ben@deciduously.com>
2020-03-18 09:26:32 -0400

    Simpler acronym

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,7 +1,7 @@
 class Acronym
   class << self
     def abbreviate(word)
-      word.split(/\W+/).inject('') { |acc, w| acc << w[0].upcase }
+      word.scan(/(\w)\w*/).join.upcase
     end
   end
 end
